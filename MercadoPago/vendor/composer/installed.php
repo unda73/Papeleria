9efe70cf056990e021345e793a4968cbb33d489f
Sergio <sergiolopezunda84@gmail.com>
2025-10-27 01:07:32 -0600

    j

--- MercadoPago/vendor/composer/installed.php
+++ MercadoPago/vendor/composer/installed.php
@@ -3,7 +3,7 @@
         'name' => '__root__',
         'pretty_version' => 'dev-main',
         'version' => 'dev-main',
-        'reference' => 'a52816afb3b41891c39675d5596fa9637cffbf04',
+        'reference' => '7b0f282c0b3462d557b98548337c3b45353ef777',
         'type' => 'library',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -13,7 +13,7 @@
         '__root__' => array(
             'pretty_version' => 'dev-main',
             'version' => 'dev-main',
-            'reference' => 'a52816afb3b41891c39675d5596fa9637cffbf04',
+            'reference' => '7b0f282c0b3462d557b98548337c3b45353ef777',
             'type' => 'library',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
